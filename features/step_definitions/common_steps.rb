9ffa2e3ba2105eb86adf686681a021d98e0697c6
Cloud Teng <cloudteng1986@gmail.com>
2011-10-15 20:50:10 +0800

    [Cloud Teng]add common steps

---
+++ features/step_definitions/common_steps.rb
@@ -0,0 +1,20 @@
+Given /^I am on "([^"]*)" homepage$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I search for "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see video results$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click one video link$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be watching this video$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
